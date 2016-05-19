dd563aa091b1214c632678237cfc7bdd07f5122e
Leland Jordon <lelandjordon@mac.com>
2016-05-19 18:51:42 -0400

    added and tested index controller.

--- public/script.js
+++ public/script.js
@@ -6,21 +6,39 @@
     "ui.router",
     "ngResource"
   ])
-  .config(Router);
+  .config(Router)
+  .controller("Index", IndexCtrl);
 
   Router.$inject = ["$stateProvider"];
   function Router($stateProvider){
     $stateProvider
     .state("index", {
-      url:         "/",
-      templateUrl: "/assets/html/salads-index.html"
+      url:          "/",
+      templateUrl:  "/assets/html/salads-index.html",
+      controller:   "Index",
+      controllerAs: "IndexVM"
     })
     .state("show", {
-      url:         "/:title",
-      templateUrl: "/assets/html/salads-show.html"
+      url:          "/:title",
+      templateUrl:  "/assets/html/salads-show.html"
     });
   }
 
+  IndexCtrl.$inject = [];
+  function IndexCtrl(){
+    var vm = this;
+    vm.salads = [
+      {
+        text: "test one"
+      },
+      {
+        text: "test two"
+      },
+      {
+        text: "Third test, Dave."
+      }
+    ];
+  }
 
 
 })();
