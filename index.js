9a15c2d78f00bed3e439a77cdc52b5cb4bbfd74d
Leland Jordon <lelandjordon@mac.com>
2016-05-19 11:23:42 -0400

    Express is working. Hello World in browser.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello Dave");
+var express = require("express");
+
+var app     = express();
+
+app.get("/", function(req, res){
+  res.send("Hello again, Dave.");
+});
+
+app.listen(4000, function(){
+  console.log("Why yes, I can do that, Dave.");
+});
