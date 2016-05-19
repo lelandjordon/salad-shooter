fd3aa9f98703c971cbd6d2c7ca838ac5a2e4b294
Leland Jordon <lelandjordon@mac.com>
2016-05-19 15:55:07 -0400

    Seeds successfully.  Adding handlebars and views.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require("express");
+var mongoose= require("./db/connection");
 
 var app     = express();
+var Salad   = mongoose.model("Salad");
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:       ".hbs",
+  layoutsDir:    "views",
+  partialsDir:   "views",
+  defaultLayout: "layout-main"
+}))
 
 app.get("/", function(req, res){
   res.send("Hello again, Dave.");
+});
+
+app.get("/api/salads", function(req, res){
+  Salad.find().then(function(salads){
+    res.json(salads);
+  });
 });
 
 app.listen(4000, function(){
